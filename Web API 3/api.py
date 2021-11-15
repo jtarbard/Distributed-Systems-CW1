62e7bf5dcb33cfdf12ea82cb3a92fa690275b27e
JTarbard <jasminetarbard@gmail.com>
2021-11-15 14:44:54 +0000

    client additions

--- Web API 3/api.py
+++ Web API 3/api.py
@@ -3,6 +3,7 @@ from types import MethodType
 from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
 import data
+import asyncio
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,22 +17,37 @@ class metacritic(Resource):
     def get(self, name):
         name = name.replace("_", " ")
         abort_if_name_doesnt_exist(name)
-        return dict(zip(data.metacritic_cols, data.get_metacritic_by_name(name)))
+        return dict(zip( data.metacritic_cols,  data.get_metacritic_by_name(name)))
 
 class steam(Resource):
     def get(self, name):
         name = name.replace("_", " ")
         abort_if_name_doesnt_exist(name)
-        return dict(zip(data.steam_cols, data.get_steam_original_price_by_name(name)))
+        return dict(zip( data.steam_cols,  data.get_steam_original_price_by_name(name)))
+
+client_parser = reqparse.RequestParser()
+client_parser.add_argument("user_id")
+client_parser.add_argument("name")
+client_parser.add_argument("comment")
+client_parser.add_argument("score")
 
 class client(Resource):
+
     def get(self, name):
         name = name.replace("_", " ")
         abort_if_name_doesnt_exist(name)
-        return dict(zip(data.client_cols, data.get_client_reviews_by_name(name)))
+        results =  data.get_client_reviews_by_name(name)
+        return dict(zip(data.client_cols, results))
 
-api.add_resource(metacritic, '/metacritic/<name>')
-api.add_resource(steam, '/steam/<name>')
+    def post(self, name):
+        args = client_parser.parse_args()
+        print("ARGS RECIEVED:", args["user_id"], name, args["comment"], args["score"])
+        data.post_client_review(args["user_id"], name, args["comment"], args["score"])
+        return print(data.client_rows)
+
+api.add_resource(metacritic, '/metacritic/<name>', methods=["GET"], endpoint="metacritic")
+api.add_resource(steam, '/steam/<name>', methods=["GET"], endpoint="steam")
+api.add_resource(client, "/client/<name>", methods=["GET", "POST"], endpoint="client")
 
 
 # Run
--- Web API 3/data.py
+++ Web API 3/data.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import asyncio
 
 # CSV storage
 metacritic_cols = []
@@ -11,17 +12,17 @@ steam_rows = []
 client_cols = []
 client_rows = []
 
-def parse_data():
+def parse_metacritic():
     global metacritic_cols, metacritic_rows
-    global steam_cols, steam_rows
-    global client_cols, client_rows
 
-    # Parse metacritic CSV into storage
     with open("Web API 3/resources/metacritic_games.csv") as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         metacritic_cols = next(reader)
         for row in reader:
             metacritic_rows.append(row)
+
+def parse_steam():
+    global steam_cols, steam_rows
 
     # Parse steam CSV into storage
     with open("Web API 3/resources/steam_games.csv") as csv_file:
@@ -30,110 +31,100 @@ def parse_data():
         for row in reader:
             steam_rows.append(row)
 
+def parse_client():
+    global client_cols, client_rows
+
     with open("Web API 3/resources/client_games.csv") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         client_cols = next(reader)
         for row in reader:
             client_rows.append(row)
+    
+    print(client_rows)
 
-parse_data()
+def parse_data():
+    parse_metacritic()
+    parse_steam()
+    parse_client()
 
 # Metacritic methods
 
 def get_metacritic_by_name(name):
+    parse_metacritic()
     metacritic_name_index = metacritic_cols.index("name")
     
     for row in metacritic_rows:
         if row[metacritic_name_index].lower() == name.lower():
             return row
+    
+    return None
 
 
 # Steam Methods
 def get_steam_original_price_by_name(name):
+    parse_steam()
     steam_name_index = steam_cols.index("name")
 
     for row in steam_rows:
         if row[steam_name_index].lower() == name.lower():
             return row
     
-    return None    
-
-# Client colums, all columns included
-client_review_id_index = client_cols.index("review_id")
-# where 'review' encompases all columns below
-client_user_id_index = client_cols.index("user_id")
-client_name_index = client_cols.index("name")
-client_comment_index = client_cols.index("comment")
-cleint_score_index = client_cols.index("score")
-client_date_index = client_cols.index("date")
+    return None
 
 # Client Methods
-def get_client_reviews_by_user_id(user_id):
-    tmp = []
-
-    for row in client_rows:
-        if row[client_user_id_index] == user_id:
-            tmp.append(row)
-    
-    return tmp
-
 def get_client_reviews_by_user_id_and_name(user_id, name):
-    tmp = []
+    parse_client()
+    client_user_id_index = client_cols.index("user_id")
+    client_name_index = client_cols.index("name")
 
     for row in client_rows:
-        if row[client_user_id_index] == user_id and row[client_name_index]== name:
-            tmp.append(row)
+        if row[client_user_id_index] == user_id and row[client_name_index].lower() == name.lower():
+            return row
     
-    return tmp
+    return False
 
 def get_client_reviews_by_name(name):
+    parse_client()
+    client_name_index = client_cols.index("name")
     tmp = []
 
     for row in client_rows:
-        if row[client_name_index]== name:
+        if row[client_name_index].lower() == name.lower():
             tmp.append(row)
-    
-    return tmp
-
-def get_client_review_score_by_name(name):
-    score = None
-    count = 0
-
-    for row in client_rows:
-        if row[client_name_index]== name:
-            score=+row[score]
-    
-    if score == None:
-        return None
-    else:
-        return score/count
+    return zip(client_cols, tmp)
 
 def post_client_review(user_id, name, comment, score):
-    #TODO: create validation
+    parse_client()
     try:
-        client_rows.append(client_rows[-1]+1, user_id, name, comment, score, datetime.now())
+        client_rows.append(user_id, name, comment, score, datetime.now())
+        new_row = [user_id, name, comment, score, datetime.now()]
+        print(client_rows, new_row)
+        with open("Web API 3/resources/client_games.csv") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(new_row)
+
         return True
     except:
         return False
 
-def update_client_review(review_id, user_id, comment, score):
-    try:
-        for row in client_rows:
-            if row[client_review_id_index] == review_id and row[client_user_id_index] == user_id:
-                row[client_comment_index] = comment
-                row[cleint_score_index] = score
-                row[client_date_index] = datetime.now()
-                return True
-        return False
-    except:
-        return False
+# def put_client_review(review_id, user_id, comment, score):
+#     try:
+#         for row in client_rows:
+#             if row[client_review_id_index] == review_id and row[client_user_id_index] == user_id:
+#                 row[client_comment_index] = comment
+#                 row[cleint_score_index] = score
+#                 row[client_date_index] = datetime.now()
+#                 return True
+#         return False
+#     except:
+#         return False
 
-def delete_client_review(review_id, user_id):
-    try:
-        for row in client_rows:
-            if row[client_review_id_index] == review_id and row[client_user_id_index] == user_id:
-                row.remove()
-                return True
-        return False
-    except:
-        return False
+# def delete_client_review(review_id, user_id):
+#     try:
+#         for row in client_rows:
+#             if row[client_review_id_index] == review_id and row[client_user_id_index] == user_id:
+#                 row.remove()
+#                 return True
+#         return False
+#     except:
+#         return False
